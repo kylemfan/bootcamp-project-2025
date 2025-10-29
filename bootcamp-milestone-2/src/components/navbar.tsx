aa2b9328ceae448357bd95ad4fce60b4cc5708b5
Kyle Fan <43425389+kylemfan@users.noreply.github.com>
2025-10-29 14:43:16 -0700

    feat: create navbar component

---
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav className={style.navbar}>
+        <h1 className={style.logo}>
+          <Link href="/">Kyle Fan | Computer Science</Link>
+        </h1>
+        <ul className={style["nav-list"]}>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/blogs">Blogs</Link></li>
+          <li><Link href="/resume">Resume</Link></li>
+          <li><Link href="/about">Contact Me</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
