80f61b2bdc492c04aa511999ea4b033b1bf1c578
Kyle Fan <43425389+kylemfan@users.noreply.github.com>
2025-10-29 20:20:35 -0700

    feat: complete home page

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Footer from "@/components/footer";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -27,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={geistMono.className}>
           <Navbar/>
+          <Footer />
         {children}
       </body>
     </html>
--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,13 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import style from "./home.module.css";
+
 export default function Home() {
   return (
     <div>
-        <main>
-          <h1 className="page-title">About Me!</h1>
-          <div className="about">
-          <div className="about-image">
-            <img src="@/public/IMG_9693.jpeg" alt="A picture of me." width="200px"/>
+      <main>
+        <h1 className={style["page-title"]}>About Me!</h1>
+        <div className={style.about}>
+          <div className={style["about-image-wrapper"]}>
+            <Image 
+              src="/images/IMG_9693.jpeg"
+              width="200"
+              height="200"
+              alt="A picture of me"
+              className={style["my-image"]}
+            />
           </div>
-          <div className="about-text">
+          <div className={style["about-text"]}>
             <p>
               Hi, my name is <strong>Kyle Fan</strong> and I study computer
               science at Cal Poly SLO!
