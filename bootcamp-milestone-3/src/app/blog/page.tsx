19b4db2adfe5ee8f5b85572750b3fbc60a872b26
Kyle Fan <43425389+kylemfan@users.noreply.github.com>
2025-11-13 11:50:53 -0800

    feat: complete blog database retrieval

--- bootcamp-milestone-3/src/app/blog/page.tsx
+++ bootcamp-milestone-3/src/app/blog/page.tsx
@@ -1,15 +1,47 @@
 import React from "react";
 import BlogPreview from "@/components/blogPreview";
-import blogs from "../blogData";
 import Link from "next/link";
+import connectDB from "@/database/db";
+import Blog from "@/database/blogSchema";
 
-export default function BlogPage() {
+async function getBlogs() {
+  await connectDB();
+
+  try {
+    const blogs = await Blog.find().sort({ date: -1 }).orFail()
+    return blogs;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+export default async function BlogPage() {
+  const blogs = await getBlogs();
+  
+  if (!blogs) {
+    return (
+      <main>
+        <h1>Blog</h1>
+        <p>Failed to load blogs. Please try again later.</p>
+      </main>
+    );
+  }
+  if (blogs.length === 0) {
+    return (
+      <main>
+        <h1>Blog</h1>
+        <p>No blogs found.</p>
+      </main>
+    );
+  }
+
   return (
     <main>
       <h1>Blog</h1>
       <div>
-        {blogs.map((blog, index) => (
-          <Link key={index} href={`/blog/${blog.slug}`}>
+        {blogs.map((blog) => (
+          <Link key={blog._id.toString()} href={`/blog/${blog.slug}`}>
             <div>{<BlogPreview {...blog} />}</div>
           </Link>
         ))}
