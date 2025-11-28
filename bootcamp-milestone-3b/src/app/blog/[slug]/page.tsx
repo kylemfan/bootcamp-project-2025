a4270a2d3e338dfb39709b228f5991544424ca55
Kyle Fan <43425389+kylemfan@users.noreply.github.com>
2025-11-28 03:27:24 -0800

    feat: add comment fetching and styling

--- bootcamp-milestone-3b/src/app/blog/[slug]/page.tsx
+++ bootcamp-milestone-3b/src/app/blog/[slug]/page.tsx
@@ -1,17 +1,19 @@
-import { NextResponse } from 'next/server';
 import connectDB from '@/database/db';
 import blogSchema from '@/database/blogSchema';
 import { notFound } from 'next/navigation';
+import Image from "next/image"
+import Comment from '@/components/comment';
+import style from "@/app/blog/[slug]/post.module.css"
 
 type IParams = {
-  params: {
+  params: Promise<{
     slug: string
-  }
+  }>;
 }
 
-export default async function GET({ params }: IParams) {
+export default async function Blog({ params }: IParams) {
   await connectDB();
-  const { slug } = params;
+  const { slug } = await params;
   const blog = await blogSchema.findOne({ slug }).orFail();
 
   if (!blog) {
@@ -20,7 +22,22 @@ export default async function GET({ params }: IParams) {
   return (
     <main>
       <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
+      <Image
+        src={blog.image}
+        alt={blog.image_alt}
+        width="300"
+        height="300"
+      />
+      <article className={style["post-article"]}>
+        <p>{blog.content}</p>
+      </article>
+      <h1>Comments</h1>
+      <div>
+        {/* TODO: ADD STYLING TO COMMENTS */}
+        {blog.comments.map((comment, index) => (
+          <Comment key={index} comment={comment}/>
+        ))}
+      </div>
     </main>
   );
 }
--- bootcamp-milestone-3b/src/database/blogSchema.ts
+++ bootcamp-milestone-3b/src/database/blogSchema.ts
@@ -1,5 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
+export type IComment = {
+  user: string;
+  content: string;
+  time: Date;
+}
+
+const commentSchema = new Schema<IComment>({
+  user: { type: String, required: true },
+  content: { type: String, required: true },
+  time: { type: Date, default: Date.now }
+})
+
 export type Blog = {
   title: string;
   slug: string;
@@ -8,17 +20,19 @@ export type Blog = {
   content: string;
   image: string;
   image_alt: string;
+  comments: IComment[];
 };
 
 const blogSchema = new Schema<Blog>(
   {
     title: { type: String, required: true },
     slug: { type: String, required: true },
-    date: { type: Date, required: false, default: new Date() },
+    date: { type: Date, required: false, default: Date.now },
     description: { type: String, required: true },
     image: { type: String, required: true },
     image_alt: { type: String, required: true },
     content: { type: String, required: true },
+    comments: { type: [commentSchema], default: [] },
   }
 );
 
